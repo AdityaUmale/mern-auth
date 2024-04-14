c07dffbfb6201027ab43052f686ee1891832982c
Aditya Umale <aditya.umale17@gmail.com>
2024-04-14 14:36:53 +0530

    header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import About from './pages/About.jsx';
 import Profile from './pages/Profile.jsx';
 import SignUp from './pages/SignUp.jsx';
 import Signin from './pages/Signin.jsx';
+import Header from './components/Header.jsx';
 
 function App() {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
