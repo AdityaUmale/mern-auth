919cb92aaa169efb2a9d56554f72d5544c4b3208
Aditya Umale <aditya.umale17@gmail.com>
2024-04-14 12:31:27 +0530

    pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+import Profile from './pages/Profile.jsx';
+import SignUp from './pages/SignUp.jsx';
+import Signin from './pages/Signin.jsx';
 
 function App() {
   return (
-    <div className='text-red-400'>App</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/sign-in' element={<Signin />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
